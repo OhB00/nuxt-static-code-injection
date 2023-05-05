c445659abf67e41eb9cd96a3ed9e74a0d233a888
OhB00 <43827372+OhB00@users.noreply.github.com>
2023-05-05 08:46:12 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Hello, world! <button>Demo!</button></div>
+</template>
+
+<script setup>
+const demo = async () => {
+  const { data } = await useFetch('/api/cached');
+};
+</script>
